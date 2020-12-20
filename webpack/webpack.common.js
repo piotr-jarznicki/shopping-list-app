2db0ec667999a125436c8a7dbcdca76d336d6da3
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-12-20 10:42:21 +0100

    Add new HTMLWebpackPlugin for add-product.html

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,35 +1,35 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Path = require("path");
+const Webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    'js/index': Path.resolve(__dirname, '../src/js/index.js'),
-    'css/index': Path.resolve(__dirname, '../src/scss/index.scss'),
+    "js/index": Path.resolve(__dirname, "../src/js/index.js"),
+    "css/index": Path.resolve(__dirname, "../src/scss/index.scss"),
   },
   output: {
-    path: Path.join(__dirname, '../dist'),
-    filename: '[name].min.js',
+    path: Path.join(__dirname, "../dist"),
+    filename: "[name].min.js",
   },
   plugins: [
     new Webpack.ProvidePlugin({
-      'mdb': 'mdb',
+      mdb: "mdb",
     }),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../src/index.html') },
-      { from: Path.resolve(__dirname, '../src/img'), to: 'img' },
+      { from: Path.resolve(__dirname, "../src/index.html") },
+      { from: Path.resolve(__dirname, "../src/img"), to: "img" },
     ]),
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].min.css',
+      filename: "[name].min.css",
     }),
   ],
   resolve: {
     alias: {
-      '~': Path.resolve(__dirname, '../src'),
-      'mdb': Path.join(__dirname, '../node_modules/mdb-ui-kit'),
+      "~": Path.resolve(__dirname, "../src"),
+      mdb: Path.join(__dirname, "../node_modules/mdb-ui-kit"),
     },
   },
   module: {
@@ -37,20 +37,25 @@ module.exports = {
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto',
+        type: "javascript/auto",
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: '[path][name].[ext]',
+            name: "[path][name].[ext]",
           },
         },
       },
       {
         test: /\.s?css/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
     ],
   },
