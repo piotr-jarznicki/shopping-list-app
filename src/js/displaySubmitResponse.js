3434aa02b096251ce5290108e4abebb075737d53
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-12-22 03:46:42 +0100

    Fix message display

--- src/js/displaySubmitResponse.js
+++ src/js/displaySubmitResponse.js
@@ -1,5 +1,9 @@
 import { submitResponse } from "./htmlHandler";
 export const displaySubmitResponse = (isFormDataValid, name) => {
+  submitResponse.style.visibility = "visible";
+  setTimeout(() => {
+    submitResponse.style.visibility = "hidden";
+  }, 2000);
   isFormDataValid
     ? (submitResponse.textContent = `Your product ${name} was added successfully!`)
     : (submitResponse.textContent = `Error, please fill out the form correctly!`);
