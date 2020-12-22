07120bfb881a57239cb1ba9a46077b614a3a85bb
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-12-22 03:05:00 +0100

    Add function for creating html elements

---
+++ src/js/createHtmlElement.js
@@ -0,0 +1,28 @@
+export const createHtmlElement = (
+  element,
+  imgSrc,
+  id,
+  cssClass,
+  children,
+  content,
+  listener
+) => {
+  const htmlElement = document.createElement(element);
+  htmlElement.classList.add(cssClass);
+  htmlElement.id = id;
+  if (imgSrc) {
+    htmlElement.src = imgSrc;
+  }
+  htmlElement.textContent = content;
+
+  if (children !== null && !Array.isArray(children)) {
+    htmlElement.appendChild(children);
+  } else if (Array.isArray(children)) {
+    children.forEach((el) => htmlElement.appendChild(el));
+  }
+  if (listener) {
+    htmlElement.addEventListener("click", listener);
+  }
+
+  return htmlElement;
+};
